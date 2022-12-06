a1d2bf7b6c62de3095f8eadc8b4437de1af8837c
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2022-12-06 17:07:33 +0100

    Aggiunto selettore livello, aggiunti livelli, esercizio base completo, colori cambiati.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
+let level_diff
 let button = document.getElementById('item-up')
 
 button.addEventListener('click', function(){
+    
     
 let grid = document.getElementById('grid');
 
 if(grid){
     grid.innerHTML = '' ;
-  }
+}
 
 function createGridSquare(number){
     const currentElement = document.createElement('div');
@@ -15,16 +17,30 @@ function createGridSquare(number){
     return currentElement;
 }
 
-function randomInteger(min, max){
-    return Math.floor(Math.random() * (max - min + 1) + min);
+level_diff = document.getElementById('level').value;
+
+
+if (level_diff == 1){
+    grid_number = 100;
+
+
 }
 
+else if (level_diff == 2){
+    grid_number = 81;
+}
 
-for(let i = 0; i<100; i++){
+else{
+    grid_number = 49;
+}
+
+for(let i = 0; i < grid_number; i++){
     const currentSquare = createGridSquare(i+1);
     grid.appendChild(currentSquare);
     currentSquare.addEventListener('click', function(){
-        this.classList.add('clicked');
+        this.classList.add('clicked')
+        console.log(`Hai cliccato il numero ${i + 1}`)
+
     })
 }
 })
